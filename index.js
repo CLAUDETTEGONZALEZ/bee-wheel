2bf951e9f3fa7adf9814207801a537a3428c2e4b
CLAUDETTEGONZALEZ <claudettegonzalezf5@gmail.com>
2022-12-15 12:04:02 +0100

    actualizar nombre array para el merge

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 document.querySelector("#btn-agregar").addEventListener("click", agregar);
 
 
-let nombres = [];
+let array_concursantes= [];
 
 function agregar() {
     let nombre = document.querySelector('#nombre').value;
 
     //agrego elemento al array
-    nombres.push(nombre);
-    console.log(nombres);
-
-    //muestro el array
-    //mostrar();
+    array_concursantes.push(nombre);
+    console.log(array_concursantes);
 
     //vaciar el input
     document.querySelector('#nombre').value = "";
 
 }
 
-/* function mostrar() {
-    let lista = document.querySelector('.listado');
-    lista.innerHTML = '';
-
-    for (let elementoActual of nombres) {
-        lista.innerHTML += "<li>" + elementoActual + "</li>";
-    }
-}
- */
