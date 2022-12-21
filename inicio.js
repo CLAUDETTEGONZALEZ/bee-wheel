4789cbd5def96faa61910318a1b457544e76f848
magda <magdalenareig@gmail.com>
2022-12-21 14:59:34 +0100

    retoques finales

--- inicio.js
+++ inicio.js
@@ -1,33 +1,37 @@
-function redireccionar (){
-    setTimeout( function() { document.location.href="http://127.0.0.1:5504/index.html" }, 4000 );
+function redirect() {
+  setTimeout(function () {
+    document.location.href = "http://127.0.0.1:5504/index.html"
+  }, 3000);
 }
 
 const dist = document.querySelector('#bee');
 
 document.querySelector('button').addEventListener('click', () => {
   dist.classList.remove('moveright');
-  setTimeout(function(){
+  setTimeout(function () {
     dist.classList.add('moveright');
-  },100);
+  }, 100);
 });
 
 var i = 0;
+
 function move() {
   if (i == 0) {
     i = 1;
-    var elem = document.getElementById("bar");  
+    var elem = document.getElementById("bar");
     var width = 10;
     var id = setInterval(frame, 15);
+
     function frame() {
       if (width >= 100) {
         clearInterval(id);
-        redireccionar();
+        redirect();
         i = 0;
       } else {
         width++;
         elem.style.width = width + "%";
-        elem.innerHTML = width  + "%";
-        
+        elem.innerHTML = width + "%";
+
       }
     }
   }
